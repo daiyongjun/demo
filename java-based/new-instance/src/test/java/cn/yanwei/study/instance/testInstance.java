2d81951488c23cc34bb7ee267f968e87d2f79aae
daiyongjun <1902717349@qq.com>
2019-12-13 10:08:42 +0800

    java-based：增加实例的创建的学习记录加代码

--- java-based/new-instance/src/test/java/cn/yanwei/study/instance/testInstance.java
+++ java-based/new-instance/src/test/java/cn/yanwei/study/instance/testInstance.java
@@ -1,54 +1,4 @@
-## 创建对象
-> [csdn博客：创建对象的5种方式](https://blog.csdn.net/m0_38016268/article/details/82957536Java "创建对象的5种方式")
-
-> [菜鸟教程：Java教程-序列化](https://www.runoob.com/java/java-serialization.html "Java教程-序列化")
-
-
-- new对象
-- 使用反射类中Class.newInstance()创建对象
-- 使用反射类中Class类中的Constructor的newInstance()方法创建对象，如:Object.class.getConstructor.newInstance()
-- 使用对象的克隆方法object.clone()方法，前提object类必须实现Cloneable类，让object类具有clone功能
-- Java序列化，java提供一种将对象序列化机制，实质就是将一个对象转换成字节序列，字节序列依然保存该对象的数据，及对象类型信息和对象属性类型等内容。同时提供反序列化机制。我们通过反序列化的ObjectInputStream中的readObject方法创建对象。前提创建对象需要实现Serializable[ˈsɪˌriəˌlaɪzəbl]接口。
-
-##### 创建pojo类，需要实现Cloneable和Serializablejie接口上
-
- ```
-package cn.yanwei.study.instance.modules;
-
-import lombok.Data;
-
-import java.io.Serializable;
-
-/**
- * pojo实例
- *
- * @author daiyongjun
- * @version 1.0
- * Created on date: 2019/12/12 10:45
- */
-@Data
-public class Employee implements Serializable, Cloneable {
-    private String name;
-
-    public Employee() {
-        System.out.println("Employee Constructor Called...");
-    }
-
-    @Override
-    public Employee clone() {
-        Employee clone = null;
-        try {
-            clone = (Employee) super.clone();
-        } catch (CloneNotSupportedException ignored) {
-
-        }
-        return clone;
-    }
-}
- ```
-##### 测试对象创建
- ```
- package cn.yanwei.study.instance;
+package cn.yanwei.study.instance;
 
 import cn.yanwei.study.instance.modules.Employee;
 import org.junit.jupiter.api.Test;
@@ -99,9 +49,8 @@ class testInstance {
         // 反序列化
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
             Employee employee4 = (Employee) ois.readObject();
+            employee4.setName("xxx4");
             System.out.println("反序列化：" + employee4);
         }
     }
 }
-
- ```
